3e25962c4d0131875b72237962073dcf1ee35aa5
felixmamaniz <semal256x@gmail.com>
2020-11-09 15:56:04 -0400

    cambios

---
+++ js/registro.js
@@ -0,0 +1,31 @@
+const nombre = document.getElementById("name")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("advertencia")
+
+form.addEventListener("submit",e=>
+{
+    e.preventDefault()
+    let advertencia = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    parrafo.innerHTML = ""
+    if(nombre.value.length < 6)
+    {
+        advertencia += `* Ingrese un nombre <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value))
+    {
+        advertencia += `* Ingrese email <br>`
+        entrar = true
+    }
+    if(entrar)
+    {
+        parrafo.innerHTML = advertencia
+    }else
+    {
+        parrafo.innerHTML = "enviado"
+    }
+});
