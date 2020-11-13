b8a442a5454bc5a8896f78370542639ecbb3b20f
Use you GibHub username <semal256x@gmail.com>
2020-11-13 16:37:58 -0400

    cambios

--- js/index.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function()
-{
-    swal('Bienvenido','Esta pagina fue creado por: Felix Mamani Zurita','success');
-});
--- js/productos.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-    setTimeout(refrescar, 10000);
-});
-function refrescar()
-{
-    location.reload();
-}
